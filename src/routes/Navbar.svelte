21944a2dab20edfb3cd10a4f01618c8bf09f843e
uhidontkno <hi@rare1k.dev>
2024-03-28 17:45:02 -0400

    feat: implement react-obfuscator in svelte

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,3 +1,9 @@
-<nav class="py-2 px-4 bg-crust/80 backdrop-blur-md">
+<script>
+    import Obfuscator from "./Obfuscator.svelte";
+
+</script>
+<nav class="py-2 px-4 bg-crust/80 backdrop-blur-md flex flex-row">
     <h1 class="navbar-h1 text-xl font-semibold">Deludo</h1>
+    <a href="index.html" class="ml-4 btn navbar-btn">Home</a>
+    <Obfuscator href="_a.html" className="ml-4 btn navbar-btn" text="Games" />
 </nav>
---
+++ src/routes/Obfuscator.svelte
@@ -0,0 +1,35 @@
+<!-- Obfuscator.svelte -->
+<script>
+    export let text = ''; // Prop for the text
+    export let href = ''; // Prop for the final href value
+    export let className = ''; // Prop for the class name
+  
+    let isHovered = false;
+  
+    // Function to handle mouse hover or click events
+    function handleHoverOrClick() {
+      isHovered = true;
+    }
+  
+    // Function to split the text into individual characters
+    function splitText(text) {
+      return text.split('');
+    }
+  
+    // Array to hold individual characters
+    let characters = splitText(text);
+  </script>
+  
+  <!-- Render each character as a <span> element -->
+    <a 
+        href={isHovered ? href : '?obf'} 
+        class={className} 
+        on:mouseover={handleHoverOrClick}
+        on:focus={handleHoverOrClick}
+        on:click={handleHoverOrClick}
+      >
+  {#each characters as character, i}
+  <span>{character}</span>
+  {/each}
+</a>
+  
