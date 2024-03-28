e6034565984fabd79592b840bde63ed12419ae48
uhidontkno <hi@rare1k.dev>
2024-03-28 16:33:29 -0400

    feat: add img alt

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,5 +5,5 @@
 
 <main class="flex flex-col w-full h-full flex-1 items-middle justify-center">
     <h1 class="text-6xl mx-auto text-center font-extrabold">{$page.status}!</h1>
-    <img src="https://http.cat/{$page.status}" style="height:200px;width:250px;" class="mt-4 mx-auto">
+    <img alt="{$page.status.toString()} error" src="https://http.cat/{$page.status}" style="height:200px;width:250px;" class="mt-4 mx-auto">
 </main>
