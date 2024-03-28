68cb922df5368bea9d3f67366de9da15ed3ca48b
uhidontkno <hi@rare1k.dev>
2024-03-28 17:46:33 -0400

    feat: tweak navbar

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -2,7 +2,7 @@
     import Obfuscator from "./Obfuscator.svelte";
 
 </script>
-<nav class="py-2 px-4 bg-crust/80 backdrop-blur-md flex flex-row">
+<nav class="py-2 px-4 bg-crust/80 backdrop-blur-md flex flex-row items-center">
     <h1 class="navbar-h1 text-xl font-semibold">Deludo</h1>
     <a href="index.html" class="ml-4 btn navbar-btn">Home</a>
     <Obfuscator href="_a.html" className="ml-4 btn navbar-btn" text="Games" />
