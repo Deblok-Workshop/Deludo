4a8e9a4dcf4b6e2f25f8bc8eec1feaab48e375c9
uhidontkno <hi@rare1k.dev>
2024-03-28 19:29:17 -0400

    feat: add hero stuff and animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
+<script>
+    import TextObfuscator from "./TextObfuscator.svelte"
+</script>
+
 <hero class="flex-1 w-full h-[100vh] bg-slate-900 flex flex-col ">
-    <div class="flex flex-col w-full h-full "></div>
+    <div class="flex flex-col w-full h-full ">
+        <h1 class="text-4xl absolute top-24 left-8">The world's <span class="soonBold">biggest</span>...</h1>
+        <sup class="text-xl absolute top-36 left-8 text-blue/70"><TextObfuscator text="Unblocked" /></sup>
+        <h2 class="text-3xl absolute top-36 left-36 text-blue font-bold">
+            <span class="alternating mr-2">test</span> site
+        </h2>
+    </div>
 </hero>
 
