0e3bf63b1be9628c8679a82cc66c08baea878d1f
uhidontkno <hi@rare1k.dev>
2024-03-28 20:22:02 -0400

    feat: i give up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,5 @@
 <script lang="ts">
 	import TextObfuscator from '@/components/TextObfuscator.svelte';
-	import { onMount } from 'svelte';
-    
-	onMount(() => {
-		function alternate() {
-			let items = ['game', 'emulator', 'flash game', 'retro game', ''];
-			let alt = document.querySelector('.alternating') as HTMLSpanElement;
-			alt.classList.add('alternating-anim');
-			setTimeout(() => {
-				alt.innerText = items[Math.floor(Math.random() * items.length)];
-			}, 1000);
-		}
-		setTimeout(() => {
-			alt.classList.remove('alternating-anim');
-		},2003);
-		setInterval(() => {
-			alternate();
-		}, 4000);
-		alternate();
-	});
 </script>
 
 <hero class="flex-1 w-full h-[100vh] bg-slate-900 flex flex-col">
@@ -26,11 +7,11 @@
 		<h1 class="text-4xl absolute top-24 left-8">
 			The world's <span class="soonBold">biggest</span>...
 		</h1>
-		<sup class="text-xl absolute top-36 left-8 text-blue/70"
-			><TextObfuscator text="Unblocked" /></sup
+		<sub class="text-xl absolute top-36 left-8 text-blue/70"
+			><TextObfuscator text="Unblocked" /></sub
 		>
 		<h2 class="text-3xl absolute top-36 left-36 text-blue font-bold">
-			<span class="alternating mr-2">test</span> site
+			<TextObfuscator text="game" /> site
 		</h2>
 	</div>
 </hero>
