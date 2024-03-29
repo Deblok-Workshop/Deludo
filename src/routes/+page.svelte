e4263beb96ea7edce5ef85587783dedc76aee364
uhidontkno <hi@rare1k.dev>
2024-03-28 20:23:03 -0400

    feat: tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,8 @@
 		<h1 class="text-4xl absolute top-24 left-8">
 			The world's <span class="soonBold">biggest</span>...
 		</h1>
-		<sub class="text-xl absolute top-36 left-8 text-blue/70"
-			><TextObfuscator text="Unblocked" /></sub
+		<span class="text-lg absolute top-36 left-10 text-blue/70"
+			><TextObfuscator text="Unblocked" /></span
 		>
 		<h2 class="text-3xl absolute top-36 left-36 text-blue font-bold">
 			<TextObfuscator text="game" /> site
