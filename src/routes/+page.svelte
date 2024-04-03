975582c4d41c625dd637c6799d0d47d063d538a5
rare1k <hi@rare1k.dev>
2024-04-03 19:24:53 -0400

    feat: change text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,10 @@
 <hero class="flex-1 w-full h-[100vh] bg-slate-900 flex flex-col">
 	<div class="flex flex-col w-full h-full">
 		<h1 class="text-4xl absolute top-24 left-8">
-			The world's <span class="soonBold">biggest</span>...
+		Welcome to <span class="soonBold"><TextObfuscator text="Deludo" />,
 		</h1>
-		<span class="text-lg absolute top-36 left-10 text-blue/70"
-			><TextObfuscator text="Unblocked" /></span
-		>
-		<h2 class="text-3xl absolute top-36 left-36 text-blue font-bold">
+		<h2 class="text-2xl absolute top-36 left-8 text-blue font-bold">
+			The world's <span class="soonBold">biggest</span>
 			<TextObfuscator text="game" /> site
 		</h2>
 	</div>
