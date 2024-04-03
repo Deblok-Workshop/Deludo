afd14e01e45c34a3519ac8c219b02a850e88ecb8
rare1k <hi@rare1k.dev>
2024-04-03 19:33:12 -0400

    feat: slide in animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
 
 <hero class="flex-1 w-full h-[100vh] bg-slate-900 flex flex-col">
 	<div class="flex flex-col w-full h-full">
-		<div class="p-8 rounded-xl bg-black/30 backdrop-blur-md absolute top-28 left-8 block">
+		<div class="p-8 rounded-xl bg-black/30 backdrop-blur-md absolute top-28 left-8 inline-block slideIn">
 		<h1 class="text-4xl ">
-		Welcome to <span class="soonBold"><TextObfuscator text="Deludo" />,
+			Welcome to <span class="soonBold"><TextObfuscator text="Deludo" />,
 		</h1>
 		<h2 class="text-2xl  text-blue font-bold">
 			The world's <span class="soonBold">biggest</span>
