244c893553b8272d20bd2cf2d44bb92b77e3bd4d
rare1k <hi@rare1k.dev>
2024-04-03 22:15:47 -0400

    fix: fix some visual glitches

--- src/routes/dragging.ts
+++ src/routes/dragging.ts
@@ -9,40 +9,38 @@ export default () => {
     document.querySelector(".bgDotContainer").style.backgroundImage = `url(${dotted})`;
     const draggables = document.querySelectorAll('.draggable');
     function startDragging(e:any) {
-// @ts-ignore
-document.querySelector(".bgDotContainer").style.backgroundImage = `url(${dotted})`;
-// @ts-ignore
-document.querySelector(".bgDotContainer").style.opacity = "0.5";
+
         console.log(e.target.classList)
         if (isDragging) {return}
         if (!e.target.classList.contains("draggable")) {return}
         e.target.classList.add("dragging")
         e.preventDefault()
+        
         isDragging = true;
         currentDraggable = e.target;
 
         if (!currentDraggable) return;
-
+// @ts-ignore
+document.querySelector(".bgDotContainer").style.opacity = "0.5";
         offsetX = e.clientX - currentDraggable.getBoundingClientRect().left;
         offsetY = e.clientY - currentDraggable.getBoundingClientRect().top;
-
-        document.addEventListener('mousemove', drag);
-        document.addEventListener('mouseup', stopDragging);
     }
 
 
     function drag(e:any) {
-        if (e.target.classList.contains("draggable")) {
-            document.querySelector(".bgDotContainer").style.opacity = "0.12";
-        }
+        
         if (!isDragging || !currentDraggable)  {
             try {
             e.target.classList.remove("dragging")
-            
+            if (e.target.classList.contains("draggable") ) {
+                document.querySelector(".bgDotContainer").style.opacity = "0.12";
+            }
             } catch {return;}
             return;
         }
-
+        if (isDragging) {
+            document.querySelector(".bgDotContainer").style.opacity = "0.5";
+        }
         currentDraggable.style.transform = `translate(${e.clientX - offsetX}px, ${e.clientY - offsetY}px)`;
     }
 
