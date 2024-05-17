e021b35371d9d1c5e205bc12e8bdfe962e2296e6
rare1k <hi@rare1k.dev>
2024-05-17 15:33:25 -0400

    feat: load the assets.json files finally

--- src/components/FSHandler.ts
+++ src/components/FSHandler.ts
@@ -1,5 +1,31 @@
+import { afterNavigate } from "$app/navigation"
+
 export default (async()=>{
 let assetsHTTP = await fetch("assets/assets.json")
 let assets = await assetsHTTP.json()
+
+// make an index
+var index = [];
+for (var x in assets) {
+   index.push(x);
+}
+
+
+console.log(assets)
+for (let i = 1; i < (assets.len+1); i++) {
+    if (!assets[index[i]][0].type || assets[index[i]][0].type != "pointer") {
+        throw new Error("Not a pointer")
+    }
+    if (!assets[index[i]][0].target) {
+        throw new Error("No url for pointer to follow")
+    }
+    let aFollowHTTP = await fetch("/assets/"+assets[index[i]][0].target)
+    let aFollow = await aFollowHTTP.json();
+    if (aFollow[index[i]] != undefined) {
+    assets[index[i]] = aFollow[index[i]]
+    }  else {
+        assets[index[i]] = aFollow
+    }
+}
 console.log(assets)
 })
