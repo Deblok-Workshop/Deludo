b811bbc195d1725fec413a9567c8d90e8a596103
rare1k <hi@rare1k.dev>
2024-05-18 14:36:07 -0400

    feat: i frogor

--- src/routes/_a/+page.svelte
+++ src/routes/_a/+page.svelte
@@ -3,7 +3,9 @@ import Card from "../../components/Card.svelte";
 import TextObfuscator from "../../components/TextObfuscator.svelte";
 import pop from "../../components/Populator.ts"
 let assets = {"html":[],"rom":{"nes":[],"snes":[],"megadrive":[]},"flash":{}}
+let len = 0;
 setTimeout(async()=>{assets = await pop();
+    len += assets.html.length
 for (let i = 0; i < assets.html.length; i++) {
     let templatecard = document.querySelector("#template");
     let card = templatecard.cloneNode(true)
@@ -12,6 +14,7 @@ for (let i = 0; i < assets.html.length; i++) {
     card.querySelector(".card-link").href = "/assets/handlers/html.html#"+assets.html[i].id
     document.querySelector(".gcontainer").appendChild(card)
     card.id = ""}
+    len += assets.flash.length
     for (let x = 0; x < assets.flash.length; x++) {
     let templatecard = document.querySelector("#template");
     let card = templatecard.cloneNode(true)
@@ -22,6 +25,7 @@ for (let i = 0; i < assets.html.length; i++) {
     card.id = ""}
 var idx= ["nes","snes","megadrive"];
 for (let y = 0; y < idx.length; y++) {
+    len += assets.rom[idx[y]].length;
 for (let b = 0; b < assets.rom[idx[y]].length; b++) {
     let templatecard = document.querySelector("#template");
     let card = templatecard.cloneNode(true)
@@ -32,6 +36,7 @@ for (let b = 0; b < assets.rom[idx[y]].length; b++) {
     card.id = ""
 }}
 document.querySelector("#template").remove();
+console.log(len)
 },33)
 </script>
 <br><br><br>
