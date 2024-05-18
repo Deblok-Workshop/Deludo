3fe6386db7c441ab6dbb36376211d74358be8e63
rare1k <hi@rare1k.dev>
2024-05-18 16:51:04 -0400

    feat: add emulator and tweaks

---
+++ src/routes/_b/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    setTimeout(()=>{
+        document.querySelector("iframe").contentDocument.body.parentElement.style.backgroundColor = "transparent";
+        document.querySelector("iframe").contentDocument.body.style.backgroundColor = "transparent";
+    },50)
+</script>
+
+<iframe title="Deludo EJS" src="/assets/handlers/ejs/index.html?threads=1" style="border:none;padding-left:8px;padding-right:8px;width:100%;height:100%;"></iframe>
