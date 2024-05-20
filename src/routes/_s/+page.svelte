ded85e12be17da5e52603fcf3a1816b4cfc4180a
rare1k <hi@rare1k.dev>
2024-05-20 19:35:11 -0400

    fix: fix settings

--- src/routes/_s/+page.svelte
+++ src/routes/_s/+page.svelte
@@ -1,8 +1,8 @@
 <script>
 import TextObfuscator from "../../components/TextObfuscator.svelte";
 </script>
-<br><br><br>
 <div class="px-2 w-full h-full">
+    <br><br><br>
 <h1 class="text-4xl">
     <span class="soonBold2"><TextObfuscator text="Deludo Settings" />
 </h1>
