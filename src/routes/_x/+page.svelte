4ea3e5a8c5b45a951579c160156f27e66dec36b7
rare1k <hi@rare1k.dev>
2024-05-25 15:29:16 -0400

    feat: start working on the actual game page

---
+++ src/routes/_x/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    
+    import Populator from "../../components/Populator";
+    
+    (async()=>{
+        let assets = await Populator()
+        console.log(assets)
+    })();
+</script>
+<br><br>
+<div class="w-full h-full flex flex-row justify-center items-center align-middle">
+    <iframe src="" class="mx-auto w-[700px] h-[480px] bg-black" title="container"></iframe>
+    <div class="ml-auto hover:w-[400px] hover:max-w-[400px] duration-300 max-w-[64px] w-[64px] overflow-clip sidepanel py-32 h-full bg-mantle rounded-l-2xl text-center">
+        <div class="duration-300 w-full h-full">
+        <h2 class="gtitle font-bold text-3xl">Testing 123</h2>
+        </div>
+    </div>
+</div>
