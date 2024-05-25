6d118e5c2963c5c4d201102be9f627ff97917fe0
rare1k <hi@rare1k.dev>
2024-05-25 15:36:42 -0400

    feat: tweaks

--- src/routes/_x/+page.svelte
+++ src/routes/_x/+page.svelte
@@ -9,9 +9,9 @@
 </script>
 <br><br>
 <div class="w-full h-full flex flex-row justify-center items-center align-middle">
-    <iframe src="" class="mx-auto w-[700px] h-[480px] bg-black" title="container"></iframe>
-    <div class="ml-auto hover:w-[400px] hover:max-w-[400px] duration-300 max-w-[64px] w-[64px] overflow-clip sidepanel py-32 h-full bg-mantle rounded-l-2xl text-center">
-        <div class="duration-300 w-full h-full">
+    <iframe src="/assets/html/000/index.html" class="mx-auto w-[calc(100%-420px)] h-[calc(100%-32px)] bg-black" title="container"></iframe>
+    <div class="ml-auto hover:w-[400px] hover:max-w-[400px] duration-500 max-w-[64px] w-[64px] overflow-clip sidepanel py-32 h-full bg-crust rounded-l-2xl text-center">
+        <div class="duration-300 w-full h-full delay-100">
         <h2 class="gtitle font-bold text-3xl">Testing 123</h2>
         </div>
     </div>
