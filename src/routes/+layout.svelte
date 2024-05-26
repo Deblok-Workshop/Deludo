64d9884d44e7fa83ed7ada6e759ebfbf8db7d02c
rare1k <hi@rare1k.dev>
2024-05-25 22:33:25 -0400

    feat: add "seo"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
     import "../tw.css";
     import Navbar from '../components/Navbar.svelte';
+    import { MetaTags } from 'svelte-meta-tags';
 </script>
+
 
 <Navbar />
 
 <slot />
+<MetaTags openGraph={{title:"Deludo"}} twitter={{title:"Deludo"}} titleTemplate="%s | Deludo"/>
 
